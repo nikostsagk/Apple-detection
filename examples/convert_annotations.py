7b1936b78b21c674323d58ac0577f8cc2dd49b5b
ntsagko <n.tsagko@gmail.com>
2019-07-10 23:54:15 +0100

    Fix convert_annotations.py
    min-x/y = 0, max-x/y = 307/201
    Also, updated data/rectangular_annotations

--- examples/convert_annotations.py
+++ examples/convert_annotations.py
@@ -4,10 +4,10 @@ import os, sys
 script_path = os.path.abspath(os.path.dirname(sys.argv[0])) # path/to/conver_annotations.py
 keras_retinanet_path = os.path.abspath(os.path.join(script_path, os.pardir)) # path/to/keras-retinanet/
 
-images_path = os.path.join(keras_retinanet_path,'data/acfr-fruit-dataset/apples/images/')
-annotations_path = os.path.join(keras_retinanet_path,'data/acfr-fruit-dataset/apples/annotations/')
-rect_annotations_path = os.path.join(keras_retinanet_path,'data/acfr-fruit-dataset/apples/rectangular_annotations/')
-sets_path = os.path.join(keras_retinanet_path,'data/acfr-fruit-dataset/apples/sets/')
+images_path = os.path.join(keras_retinanet_path,'data/images/')
+annotations_path = os.path.join(keras_retinanet_path,'data/annotations/')
+rect_annotations_path = os.path.join(keras_retinanet_path,'data/rectangular_annotations/')
+sets_path = os.path.join(keras_retinanet_path,'data/sets/')
 
 #create directory
 if not os.path.exists(rect_annotations_path):
@@ -31,7 +31,7 @@ for root, dirs, files in os.walk(annotations_path):
     for file in files:
         if file.endswith('.csv'):
             with open(os.path.join(annotations_path, file)) as f:
-                empty = True if (sum(1 for line in f) == 1) else False #check if .csv is empty
+                empty = True if (sum(1 for line in f) == 1) else False #check if .csv is empty # Needs to be fixed
                 
             if empty:
                 row = [os.path.join(images_path,file.replace('csv','png')),'','','','','']
@@ -43,17 +43,29 @@ for root, dirs, files in os.walk(annotations_path):
                     reader = csv.reader(f)
                     next(reader)
                     for row in reader:
-                        row[0] = os.path.join(images_path, file.replace('csv','png'))
+                        row[0] = os.path.join(images_path, file.replace('csv','png')) # path
                         radius = float(row[3])
-                        row[1] = round(float(row[1]) - radius) 
-                        row[2] = round(float(row[2]) - radius)  
-                        row[3] = round(float(row[1]) + 2*radius)
-                        row[4] = round(float(row[2]) + 2*radius)
-                        row.append('apple')
-                        if (row[1] >= 0) and (row[2] >= 0) and (row[3] <= 308) and (row[4] <= 202):
-                            for key in data:
+                        row[1] = round(float(row[1]) - radius) # x0 
+                        row[2] = round(float(row[2]) - radius) # y0
+                        row[3] = round(float(row[1]) + 2*radius) # x1
+                        row[4] = round(float(row[2]) + 2*radius) # y1
+                        row.append('apple') # class
+
+                        # Fix boxes on the fringe
+                        if (row[1] < 0) : row[1] = 0
+                        if (row[2] < 0) : row[2] = 0
+                        if (row[3] > 307) : row[3] = 307
+                        if (row[4] > 201) : row[4] = 201
+
+                        for key in data:
                                 if is_in_set(key, file.replace('.csv',''), sets_path) == True:
                                     data[key].append(row)
+
+                        # Discard annotations that are off the image
+                        #if (row[1] >= 0) and (row[2] >= 0) and (row[3] <= 307) and (row[4] <= 201):
+                        #    for key in data:
+                        #        if is_in_set(key, file.replace('.csv',''), sets_path) == True:
+                        #            data[key].append(row)
 
                     
 annotation_sets = {}
