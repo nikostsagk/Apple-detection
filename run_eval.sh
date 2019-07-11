c919c3f175e32cbcd0e8f9c6b4bbe89dd72e557f
N.C.Tsagkopoulos <ncdt1u18@cyan51.cluster.local>
2019-07-11 11:55:32 +0100

    update

--- run_eval.sh
+++ run_eval.sh
@@ -1,5 +1,12 @@
- python ./keras_retinanet/bin/evaluate.py  --backbone 'resnet152' --iou-threshold 0.5 --convert-model --save-path ./dummy_predictions --score-threshold 0.99 \
+#!/bin/bash
+
+#SBATCH -p lyceum
+
+#SBATCH --time=60:00:00          # walltime
+#SBATCH --gres=gpu:1
+ 
+python ./keras_retinanet/bin/evaluate.py  --backbone 'resnet152' --iou-threshold 0.5 --convert-model --save-path ./dummy_predictions --score-threshold 0.99 \
  											csv ./data/acfr-fruit-dataset/apples/rectangular_annotations/dummy_val_annotations.csv ./data/acfr-fruit-dataset/apples/rectangular_annotations/classes.csv \
- 											./resnet152/frozen/resnet152_csv_10.h5
+ 											./resnet152_csv_10.h5
  											
  										
