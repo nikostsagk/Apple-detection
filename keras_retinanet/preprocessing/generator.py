66d9c8a444e6a430ae2bc5df82935a6c326b3f36
ntsagko <n.tsagko@gmail.com>
2019-07-16 19:53:24 +0100

    Supress bbox warning (generator.py line:170)

--- keras_retinanet/preprocessing/generator.py
+++ keras_retinanet/preprocessing/generator.py
@@ -164,11 +164,11 @@ class Generator(keras.utils.Sequence):
 
             # delete invalid indices
             if len(invalid_indices):
-                warnings.warn('Image with id {} (shape {}) contains the following invalid boxes: {}.'.format(
-                    group[index],
-                    image.shape,
-                    annotations['bboxes'][invalid_indices, :]
-                ))
+                #warnings.warn('Image with id {} (shape {}) contains the following invalid boxes: {}.'.format(
+                #    group[index],
+                #    image.shape,
+                #    annotations['bboxes'][invalid_indices, :]
+                #))
                 for k in annotations_group[index].keys():
                     annotations_group[index][k] = np.delete(annotations[k], invalid_indices, axis=0)
 
