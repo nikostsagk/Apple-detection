3823928850e48384e7a46473a71f04d0d81663c1
ntsagko <n.tsagko@gmail.com>
2019-07-16 01:12:13 +0100

    Remove regularizer

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -114,18 +114,9 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
         from keras.utils import multi_gpu_model
         with tf.device('/cpu:0'):
             model = model_with_weights(backbone_retinanet(num_classes, num_anchors=num_anchors, modifier=modifier), weights=weights, skip_mismatch=True)
-            
-            # Add weight decay (L2 regularization)
-            for layer in model.layers:
-                layers.kernel_regularizer = keras.regularizers.l2(5e-6)
         training_model = multi_gpu_model(model, gpus=multi_gpu)
     else:
         model          = model_with_weights(backbone_retinanet(num_classes, num_anchors=num_anchors, modifier=modifier), weights=weights, skip_mismatch=True)
-        
-        # Add weight decay (L2 regularization)
-        for layer in model.layers:
-            layers.kernel_regularizer = keras.regularizers.l2(5e-6)
-
         training_model = model
 
     # make prediction model
