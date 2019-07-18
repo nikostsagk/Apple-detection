9df73621022cc780bdc46e236290f418ecda271f
ntsagko <n.tsagko@gmail.com>
2019-07-18 11:37:11 +0100

    hahaha

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -239,11 +239,11 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
     ))
 
     if args.lr_schedule:
-    lrate = LearningRateScheduler(default_lr_scheduler(
-        base_lr=args.lr,
-        steps=np.array([args.steps*6,args.steps*10]), verbose=1)
-    )
-    callbacks.append(lrate)
+        lrate = LearningRateScheduler(default_lr_scheduler(
+            base_lr=args.lr,
+            steps=np.array([args.steps*6,args.steps*10]), verbose=1)
+        )
+        callbacks.append(lrate)
 
     return callbacks
 
