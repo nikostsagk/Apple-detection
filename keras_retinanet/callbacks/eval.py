5157969d0142bde056486ceea23691e565e02563
ntsagko <n.tsagko@gmail.com>
2019-07-18 13:09:43 +0100

    Clean up code

--- keras_retinanet/callbacks/eval.py
+++ keras_retinanet/callbacks/eval.py
@@ -68,21 +68,15 @@ class Evaluate(keras.callbacks.Callback):
             max_detections=self.max_detections,
             save_path=self.save_path
         )
-        print(average_precisions)
-        print(average_f1_scores)
-        print(true_positives)
-        print(false_positives)
-        print(precision)
-        print(recall)
-        print(f1_score)
 
         # compute per class average precision
         total_instances = []
         precisions = []
         for label, (average_precision, num_annotations ) in average_precisions.items():
             if self.verbose == 1:
-                print('{:.0f} instances of class'.format(num_annotations),
-                      self.generator.label_to_name(label), 'with average precision: {:.4f}'.format(average_precision))
+                # commented because they are printed in the end
+                #print('{:.0f} instances of class'.format(num_annotations),
+                #      self.generator.label_to_name(label), 'with average precision: {:.4f}'.format(average_precision))
             total_instances.append(num_annotations)
             precisions.append(average_precision)
 
@@ -90,8 +84,9 @@ class Evaluate(keras.callbacks.Callback):
         f1_scores = []
         for label, (average_f1_score, num_annotations) in average_f1_scores.items():
             if self.verbose == 1:
-                print('{:.0f} instances of class'.format(num_annotations),
-                      self.generator.label_to_name(label), 'with average F1-score: {:.4f} \n'.format(average_f1_score))
+                # commented because they are printed in the end
+                #print('{:.0f} instances of class'.format(num_annotations),
+                #      self.generator.label_to_name(label), 'with average F1-score: {:.4f} \n'.format(average_f1_score))
             f1_scores.append(average_f1_score)
 
         if self.weighted_average:
