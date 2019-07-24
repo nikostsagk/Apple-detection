5d6e81a3a3705ecb638d1c7e11d4d53d80ba705e
ntsagko <n.tsagko@gmail.com>
2019-07-24 23:55:38 +0100

    New anchor computations

--- keras_retinanet/models/retinanet.py
+++ keras_retinanet/models/retinanet.py
@@ -207,11 +207,12 @@ def __build_pyramid(models, features):
     return [__build_model_pyramid(n, m, features) for n, m in models]
 
 
-def __build_anchors(anchor_parameters, features):
+def __build_anchors(anchor_parameters, image, features):
     """ Builds anchors for the shape of the features from FPN.
 
     Args
         anchor_parameters : Parameteres that determine how anchors are generated.
+        image             : The image input tensor.
         features          : The FPN features.
 
     Returns
@@ -229,7 +230,7 @@ def __build_anchors(anchor_parameters, features):
             ratios=anchor_parameters.ratios,
             scales=anchor_parameters.scales,
             name='anchors_{}'.format(i)
-        )(f) for i, f in enumerate(features)
+        )([image, f]) for i, f in enumerate(features)
     ]
 
     return keras.layers.Concatenate(axis=1, name='anchors')(anchors)
@@ -288,9 +289,6 @@ def retinanet_bbox(
     model                 = None,
     nms                   = True,
     class_specific_filter = True,
-    nms_threshold         = 0.5,
-    score_threshold       = 0.05,
-    max_detections        = 300,
     name                  = 'retinanet-bbox',
     anchor_params         = None,
     **kwargs
@@ -331,7 +329,7 @@ def retinanet_bbox(
 
     # compute the anchors
     features = [model.get_layer(p_name).output for p_name in ['P3', 'P4', 'P5', 'P6', 'P7']]
-    anchors  = __build_anchors(anchor_params, features)
+    anchors  = __build_anchors(anchor_params, model.inputs[0], features)
 
     # we expect the anchors, regression and classification values as first output
     regression     = model.outputs[0]
@@ -348,9 +346,6 @@ def retinanet_bbox(
     detections = layers.FilterDetections(
         nms                   = nms,
         class_specific_filter = class_specific_filter,
-        nms_threshold         = nms_threshold,
-        score_threshold       = score_threshold,
-        max_detections        = max_detections,
         name                  = 'filtered_detections'
     )([boxes, classification] + other)
 
