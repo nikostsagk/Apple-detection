e9802ef38249a6208fb8789447e06375d0de2bc5
ntsagko <n.tsagko@gmail.com>
2019-08-01 23:19:10 +0100

    implemented VGG11 and VGG13

--- keras_retinanet/models/vgg.py
+++ keras_retinanet/models/vgg.py
@@ -36,7 +36,7 @@ class VGGBackbone(Backbone):
         """ Downloads ImageNet weights and returns path to weights file.
         Weights can be downloaded at https://github.com/fizyr/keras-models/releases .
         """
-        if self.backbone == 'vgg16':
+        if self.backbone == 'vgg16' or self.backbone == 'vgg13' or self.backbone == 'vgg11' :
             resource = keras.applications.vgg16.vgg16.WEIGHTS_PATH_NO_TOP
             checksum = '6d6bbae143d832006294945121d1f1fc'
         elif self.backbone == 'vgg19':
@@ -55,7 +55,7 @@ class VGGBackbone(Backbone):
     def validate(self):
         """ Checks whether the backbone string is correct.
         """
-        allowed_backbones = ['vgg16', 'vgg19']
+        allowed_backbones = ['vgg16', 'vgg19', 'vgg13', 'vgg11']
 
         if self.backbone not in allowed_backbones:
             raise ValueError('Backbone (\'{}\') not in allowed backbones ({}).'.format(self.backbone, allowed_backbones))
@@ -87,6 +87,10 @@ def vgg_retinanet(num_classes, backbone='vgg16', inputs=None, modifier=None, **k
         vgg = keras.applications.VGG16(input_tensor=inputs, include_top=False, weights=None)
     elif backbone == 'vgg19':
         vgg = keras.applications.VGG19(input_tensor=inputs, include_top=False, weights=None)
+    elif backbone == 'vgg13':
+        vgg = vgg13(input_tensor=inputs, weights=None)
+    elif backbone == 'vgg11':
+        vgg = vgg11(input_tensor=inputs, weights=None)
     else:
         raise ValueError("Backbone '{}' not recognized.".format(backbone))
 
@@ -95,5 +99,69 @@ def vgg_retinanet(num_classes, backbone='vgg16', inputs=None, modifier=None, **k
 
     # create the full model
     layer_names = ["block3_pool", "block4_pool", "block5_pool"]
-    layer_outputs = [vgg.get_layer(name).output for name in layer_names]
+    layer_outputs = [vgg.get_layer(name).get_output_at(0) for name in layer_names]
     return retinanet.retinanet(inputs=inputs, num_classes=num_classes, backbone_layers=layer_outputs, **kwargs)
+
+
+def vgg13(input_tensor=None, weights=None):
+    """
+    Returns
+        A lighter version of VGG16.
+    """
+    # Block 1
+    x = keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(input_tensor)
+    x = keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    return keras.Model(input_tensor, x, name='VGG13')
+
+def vgg11(input_tensor=None, weights=None):
+    """
+    Returns
+        A lighter version of VGG16.
+    """
+    # Block 1
+    x = keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(input_tensor)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    return keras.Model(input_tensor, x, name='VGG11')
+
