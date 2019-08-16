364ade483e6dda3b2b96e8477acee406a5fbbc14
ntsagko <n.tsagko@gmail.com>
2019-08-16 16:50:27 +0100

    Fix anchors

--- keras_retinanet/utils/anchors.py
+++ keras_retinanet/utils/anchors.py
@@ -44,7 +44,7 @@ The default anchor parameters.
 """
 AnchorParameters.default = AnchorParameters(
     sizes   = [32, 64, 128],
-    strides = [8, 16, 32, 64, 128],
+    strides = [8, 16, 32],
     ratios  = np.array([0.5, 1, 2], keras.backend.floatx()),
     scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], keras.backend.floatx()),
 )
